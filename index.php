950fa4835977a0f420afd56fcc1663d4d9a16dd5
furPedroRibeiro <84580101+furPedroRibeiro@users.noreply.github.com>
2023-03-31 11:34:41 -0300

    cursosLivresWordpress

---
+++ index.php
@@ -0,0 +1,19 @@
+    <?php
+        get_header();
+    ?>
+    <header>
+        <span>Logo</span>
+        <a href=""></a>
+        <a href=""></a>
+        <a href=""></a>
+        <a href=""></a>
+    </header>
+    <main>
+
+    </main>
+    <footer>
+
+    </footer>
+    <?php
+        get_footer();
+    ?>
